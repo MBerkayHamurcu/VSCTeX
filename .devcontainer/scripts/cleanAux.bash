c9a7fc7b4f0f27b4b16f0dea0171c2e4c808cc83
MBerkayHamurcu <berkay.hamurcu@web.de>
2024-02-08 20:20:37 +0100

    Template and container settings improvements - Removed the last `.` character of title numberings - Removed duplicate toc entry for the list of equations - Removed duplicate text in '30anotherMainPart.tex' - Set VS Code editor word wrap to 80 columns - Set 'latex-workshop watch pdf delay' to 3 seconds - Renamed 'aux' folder to 'auxiliary' (for aux is an invalid directory name in Windows)

--- .devcontainer/scripts/cleanAux.bash
+++ .devcontainer/scripts/cleanAux.bash
@@ -2,6 +2,6 @@
 
 latexmk -c
 
-rm -r aux indent.log main.acr main.lol main.lolistedEquation main.tdo || true
+rm -r auxiliary indent.log main.acr main.lol main.lolistedEquation main.tdo || true
 
 echo "Cleaning auxiliary files was successfull."
--- .devcontainer/scripts/generateDocFast.bash
+++ .devcontainer/scripts/generateDocFast.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-[[ -d aux ]] || mkdir aux
+[[ -d auxiliary ]] || mkdir auxiliary
 
-pdflatex -synctex=1 -halt-on-error -output-directory=aux main.tex && mv aux/main.pdf main.pdf && mv aux/main.synctex.gz main.synctex.gz 
+pdflatex -synctex=1 -halt-on-error -output-directory=auxiliary main.tex && mv auxiliary/main.pdf main.pdf && mv auxiliary/main.synctex.gz main.synctex.gz 
