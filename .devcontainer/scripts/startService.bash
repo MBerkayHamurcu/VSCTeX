8d9088604977d20c584839ff17d6959406d4b641
MBerkayHamurcu <berkay.hamurcu@web.de>
2024-06-06 16:26:02 +0200

    More stable dev container creation and boot

--- .devcontainer/scripts/startService.bash
+++ .devcontainer/scripts/startService.bash
@@ -4,10 +4,24 @@ echo -e "\n------startService.bash------\n"
 
 service devContainerService start
 
-# Check if the service started successfully
 if [ "$?" -ne 0 ]; then
-    echo -e "Failed to start the dev container service.\n"
-    exit 0
+  echo -e "Failed to start the Dev Container service.\n"
+else
+  echo -e "Successfully started the Dev Container service.\n"
 fi
 
-echo -e "Successfully started the dev container service.\n"
+if [ -f "/workspaces/VSCTeX/.devcontainer/DevContainerIsPostCreate" ]; then
+  cd /workspaces/VSCTeX
+
+  echo -e "Starting initial document compilation...\n"
+
+  latexmk -interaction=nonstopmode -synctex=1 -file-line-error -pdf -view=none -aux-directory=auxiliary -pdflatex='pdflatex %O "\def\fullCompile{} \input{%S}"' main.tex
+
+  if [ "$?" -ne 0 ]; then
+    echo -e "\n\nFailed to compile the document: The Dev Container is ready to be used but the document contains compilation errors.\n"
+  else
+    echo -e "\n\nSuccessfully compiled the document: The Dev Container is ready to be used.\n"
+  fi
+
+  rm -f /workspaces/VSCTeX/.devcontainer/DevContainerIsPostCreate
+fi
