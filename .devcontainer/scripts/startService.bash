4953014e2e64c5aa5333f47e8cfb98a5fef9faef
MBerkayHamurcu <berkay.hamurcu@web.de>
2024-06-06 17:41:37 +0200

    Added support for arbitrary parent directory name

--- .devcontainer/scripts/startService.bash
+++ .devcontainer/scripts/startService.bash
@@ -2,6 +2,8 @@
 
 echo -e "\n------startService.bash------\n"
 
+sleep 5
+
 service devContainerService start
 
 if [ "$?" -ne 0 ]; then
@@ -10,18 +12,23 @@ else
   echo -e "Successfully started the Dev Container service.\n"
 fi
 
-if [ -f "/workspaces/VSCTeX/.devcontainer/DevContainerIsPostCreate" ]; then
-  cd /workspaces/VSCTeX
+for dir in /workspaces/*/;
+do
+  if [ -f "${dir}.devcontainer/DevContainerIsPostCreate" ]; then
+    cd /workspaces/*/
 
-  echo -e "Starting initial document compilation...\n"
+    echo -e "Starting initial document compilation...\n"
 
-  latexmk -interaction=nonstopmode -synctex=1 -file-line-error -pdf -view=none -aux-directory=auxiliary -pdflatex='pdflatex %O "\def\fullCompile{} \input{%S}"' main.tex
+    latexmk -interaction=nonstopmode -synctex=1 -file-line-error -pdf -view=none -aux-directory=auxiliary -pdflatex='pdflatex %O "\def\fullCompile{} \input{%S}"' main.tex
 
-  if [ "$?" -ne 0 ]; then
-    echo -e "\n\nFailed to compile the document: The Dev Container is ready to be used but the document contains compilation errors.\n"
-  else
-    echo -e "\n\nSuccessfully compiled the document: The Dev Container is ready to be used.\n"
+    if [ "$?" -ne 0 ]; then
+      echo -e "\n\nFailed to compile the document: The Dev Container is ready to be used but the document contains compilation errors.\n"
+    else
+      echo -e "\n\nSuccessfully compiled the document: The Dev Container is ready to be used.\n"
+    fi
+
+    rm -f /workspaces/*/.devcontainer/DevContainerIsPostCreate
+
+    break
   fi
-
-  rm -f /workspaces/VSCTeX/.devcontainer/DevContainerIsPostCreate
-fi
+done
