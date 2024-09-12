97ea399a6ed740bec221194b64b9447baaf9c203
Berkay Hamurcu <Berkay.Hamurcu@web.de>
2024-09-12 23:48:44 +0200

    Fixed comments in setWSLConfig.bat

--- .devcontainer/setWSLConfig.bat
+++ .devcontainer/setWSLConfig.bat
@@ -4,14 +4,14 @@ IF NOT EXIST .wslconfig (
     echo # Settings apply across all Linux distros running on WSL ^2>.wslconfig
     echo [wsl2]>>.wslconfig
     echo.>>.wslconfig
-    echo # Limits VM memory to use no more than ^4 GB, this can be set as whole numbers using GB or MB>>.wslconfig
+    echo # Limits VM memory to use no more than ^3 GB, this can be set as whole numbers using GB or MB>>.wslconfig
     echo memory=3GB>>.wslconfig
     echo.>>.wslconfig
-    echo # Sets the VM to use four virtual processors>>.wslconfig
+    echo # Sets the VM to use ^2 virtual processors>>.wslconfig
     echo processors=^2>>.wslconfig
     echo.>>.wslconfig
-    echo # Sets amount of swap storage space to ^0GB, default is ^25 percent of available RAM>>.wslconfig
-    echo swap=8GB>>.wslconfig
+    echo # Sets amount of swap storage space to ^12GB>>.wslconfig
+    echo swap=12GB>>.wslconfig
     echo.>>.wslconfig
     echo # Turn off default connection to bind WSL ^2 localhost to Windows localhost>>.wslconfig
     echo localhostforwarding=false>>.wslconfig
