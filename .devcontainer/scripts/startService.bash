7b4d94845428e0355210b680f7cd0ef0616bb183
Berkay Hamurcu <Berkay.Hamurcu@web.de>
2024-12-30 18:37:31 +0100

    Let the LTeX extension be installed automatically Some issues in the past like the LTeX language server not being able to find the Java installation on Apple M-chip devices were solved by installing the extension manually (bash script). This method turns out to cause a whole bunch of problems itself. Future issues with this extension will be solved differently.

--- .devcontainer/scripts/startService.bash
+++ .devcontainer/scripts/startService.bash
@@ -10,28 +10,32 @@ if [ "$?" -ne 0 ]; then
   exit 1
 fi
 
+latexmk --version >/dev/null 2>&1
+
+if [ "$?" -ne 0 ]; then
+  echo "VSCTeX error (STS1). Please report this incident to the maintainer."
+  bash .devcontainer/scripts/log.bash startService E0x01
+  exit 1
+fi
+
+if [ -f ".devcontainer/DevContainerIsPostCreate" ]; then
+  echo -e "Starting initial document compilation...\n"
+  bash .devcontainer/scripts/log.bash startService DevContainerIsPostCreate
+
+  latexmk -interaction=nonstopmode -synctex=1 -file-line-error -pdf -view=none -aux-directory=auxiliary -pdflatex='pdflatex %O "\def\fullCompile{} \input{%S}"' main.tex
+
+  if [ "$?" -ne 0 ]; then
+    echo -e "\n\nFailed to compile the document. VSCTeX is ready to be used but the document contains compilation errors.\n"
+    rm main.pdf main.synctex.gz
+  else
+    echo -e "\n\nSuccessfully compiled the document.\n"
+  fi
+
+  rm /workspaces/*/.devcontainer/DevContainerIsPostCreate
+fi
+
 bash .devcontainer/scripts/log.bash startService START_CONTAINER
 
 echo -e "Successfully started VSCTeX.\n"
 
-if [ ! -f ".devcontainer/DevContainerIsPostCreate" ]; then
-  nohup bash -c 'bash scripts/watchFileChanges.bash &' >/dev/null 2>&1
-  exit 0
-fi
-
-echo -e "Starting initial document compilation...\n"
-
-latexmk -interaction=nonstopmode -synctex=1 -file-line-error -pdf -view=none -aux-directory=auxiliary -pdflatex='pdflatex %O "\def\fullCompile{} \input{%S}"' main.tex
-
-if [ "$?" -ne 0 ]; then
-  echo -e "\n\nFailed to compile the document. VSCTeX is ready to be used but the document contains compilation errors.\n"
-  rm main.pdf main.synctex.gz
-else
-  echo -e "\n\nSuccessfully compiled the document.\n"
-fi
-
-rm /workspaces/*/.devcontainer/DevContainerIsPostCreate
-
-echo -e "Installing the VS Code LTeX extension (grammar & spell checker)...\n"
-
-exec bash .devcontainer/scripts/installLTeX.bash
+nohup bash -c 'bash .devcontainer/scripts/watchFileChanges.bash &' >/dev/null 2>&1
